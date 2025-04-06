02dc49d3f4c8d8d5461b1c1ca046ebd8327f20c6
UltraTeamC <UltraTeamC@outlook.com>
2025-04-06 07:58:35 +0000

    init

---
+++ Header/Terminal/new.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2025 Ultrasoft. All rights reserved.
+
+    This software is the confidential and proprietary information of
+    Ultrasoft. ("Confidential Information"). You shall not
+    disclose such Confidential Information and shall use it only in
+    accordance with the terms of the license agreement you entered into
+    with Ultrasoft.
+*/
+
