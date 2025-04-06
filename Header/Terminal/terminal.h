82754b742b4a0ff20a9d3eaee0f834ced7d44fc1
UltraTeamC <UltraTeamC@outlook.com>
2025-04-06 08:45:19 +0000

    init

---
+++ Header/Terminal/terminal.h
@@ -0,0 +1,7 @@
+#include "../21c-kernel/Header/stdio.h"
+
+int main() {
+    int version = 0;
+    printf("Ultrasoft 21c-kernel ver[%d]\n(c) 2025 Ultrasoft\n",version);
+    return 0;
+}
--- Terminal/System/terminal.c
+++ Terminal/System/terminal.c
@@ -1,7 +1,1 @@
-#include "../21c-kernel/Header/stdio.h"
-
-int main() {
-    int version = 0;
-    printf("Ultrasoft 21c-kernel ver[%d]\n(c) 2025 Ultrasoft",version);
-    return 0;
-}
+#include "../21c-kernel/Header/Terminal/terminal.h"
