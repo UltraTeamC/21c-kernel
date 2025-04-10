67f713dd4be01b0bb8c0753efaefcfe0181177c3
UltraTeamC <UltraTeamC@outlook.com>
2025-04-10 12:03:33 +0000

    a

--- System/System.c
+++ System/System.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-#define PATH_MAX 256
+#define PATH_MAX 512
 #define PATH_MIN 1
 
 int main() {
     printf(System);
     return 0;
-}
-void System(data,path) {
-    // System Data:
-    float version   = 0.11;
-    char  build[10] = "0.11.0"
-    // System Path: 
-    char System[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/System";
-    char Terminal[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/Terminal";
+    void System(data,path) {
+        // System Data:
+        float version   = 0.11;
+        char  build[10] = "0.11.0"
+        // System Path: 
+        char System[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/System";
+        char Terminal[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/Terminal";
+    }
 }
