08731f574ebd794b87c622afd67aa9fa638550d4
UltraTeamC <UltraTeamC@outlook.com>
2025-04-12 11:41:21 +0000

    a

---
+++ Header/definition.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/* System Path */
+#define PATH_MAX 512
+#define PATH_MIN 1
+#define BOOTLOAD[PATH_MAX] "(A):Bootloader/"
+#define USERPATH[PATH_MAX] "(A):User/"
+#define SYSTEM[PATH_MAX]   "(A):System/"
+#define HERDER[PATH_MAX]   "(A):Herder/"
+#define TERMINAL[PATH_MAX] "(A):Terminal/"
+
+
+/* System Error */
+#define ERROR_001 "System crash."
+#define ERROR_002 "Critical system files are missing."
+#define ERROR_003 "Vical."
--- System/System.c
+++ System/System.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-#define PATH_MAX  512
-#define PATH_MIN  1
-#define ERROR_001 "System crash."
-#define ERROR_002 "Critical system files are missing."
-#define ERROR_003 "Vical."
-
 int main() {
     printf("Ultrasoft[0.0.1]\n");
     // Debug
@@ -14,13 +8,3 @@ int main() {
     printf("%s\n",ERROR_003);
     return 0;
 }
-// void System(data,path) {
-//     // System Data:
-//     float version = 0.11;
-//     char  build[10] = "0.11.0"
-//     // System Path: 
-//     char System[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/System";
-//     char Terminal[PATH_MAX] = "[a]:User/Kernel/21c-Kernel/Terminal";
-//     // System
-//     return 0;
-// }
