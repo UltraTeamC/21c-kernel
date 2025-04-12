d7065f01c996c6aaccba1a7af90f065240f42ac8
UltraTeamC <UltraTeamC@outlook.com>
2025-04-12 12:29:27 +0000

    ERROR

--- System/System.c
+++ System/System.c
@@ -15,21 +15,16 @@ int main() {
     return 0;
 }
 void System_Error() {
-    while (5>3):{
-        if (System_status = false)
-        {
-            if (System_Error = 1) {
-                printf("The system has crashed with the error code:%s\n",ERROR_001);
-                return "ERROR = 1";
+    while (5>3) {
+        if (System_status == 1) { // 检查系统状态是否出错
+            switch (System_Error) {
+                case 1:
+                    printf("The system has crashed with the error code: %s\n", ERROR_001);
+                case 2:
+                    printf("The system has crashed with the error code: %s\n", ERROR_002);
+                default:
+                    printf("The system has crashed with the error code: %s\n", ERROR_003);
             }
-            else if (System_Error = 2) {
-                printf("The system has crashed with the error code:%s\n",ERROR_002);
-                return "ERROR = 2";
-            }
-            else {
-                printf("The system has crashed with the error code:%s\n",ERROR_003);
-                return "ERROR = 3";
-            }            
-        }
+        }            
     }
 }
